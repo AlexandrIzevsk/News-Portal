ac38d044edc5ff8234c047ad6ed422c10bec2e3a
AlexandrIzevsk <sasha-sarov-333@rambler.ru>
2024-12-13 02:06:02 +0400

    News Portal: add new functions for admin panel

--- News_Portal/NewsPORTAL/admin.py
+++ News_Portal/NewsPORTAL/admin.py
@@ -2,8 +2,39 @@ from django.contrib import admin
 from .models import Author, Category, Post, PostCategory, Comment
 
 
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(PostCategory)
+def nulfy_rating(modeladmin, request, queryset):
+    queryset.update(rating=0)
+    nulfy_rating.short_description = 'Обнулить товары'
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating')
+    list_filter = ('rating',)
+    search_fields = ('user__username',)
+    actions = [nulfy_rating]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'choice', 'author', 'rating', 'time_in')
+    list_filter = ('author__user__username', 'rating',)
+    search_fields = ('author__user__username',)
+    actions = [nulfy_rating]
+
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    list_display = ('post', 'category')
+    list_filter = ('post__choice', 'post__author__user__username')
+    search_fields = ('category__name','post__author__user__username')
+    actions = [nulfy_rating]
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostCategory, PostCategoryAdmin)
 admin.site.register(Comment)
